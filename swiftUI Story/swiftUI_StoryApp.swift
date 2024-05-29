7af46fdda59ff7dae9806a82969e8649ee038b8b
Han <g43424140@gmail.com>
2024-05-29 17:06:30 +0800

    Initial Commit

---
+++ swiftUI Story/swiftUI_StoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_StoryApp.swift
+//  swiftUI Story
+//
+//  Created by YaHan on 2024/5/29.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_StoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
